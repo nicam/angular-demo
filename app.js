d389338b047e0e6f7d9aef607aeacc5384ecbe50
Pascal Helfenstein <pascal.helfenstein@gmail.com>
2014-08-25 20:20:34 +0200

    step6

---
+++ app.js
@@ -0,0 +1,20 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('TestController', function ($scope) {
+  $scope.persons = [
+    { name: 'Pascal', location: 'Wil' },
+    { name: 'Peter', location: 'St. Gallen' },
+    { name: 'Hans', location: 'Ebenalp' }
+  ];
+
+  $scope.addPerson = function() {
+    $scope.persons.push({
+      name: $scope.name,
+      location: $scope.location
+    });
+  }
+
+  $scope.removePerson = function(person) {
+    $scope.persons.splice($scope.persons.indexOf(person), 1);
+  }
+});
